331f00c4fa63ead15b0d9977cc8c4acc684b8744
Eden Kupermintz <edenk@liveperson.com)>
2020-08-06 00:14:48 +0300

    caret + icon position

--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -9,9 +9,37 @@ function btnHandler(selector, callback) {
 }
 
 function contentCollapse (event) {
-  var content = event.target.nextElementSibling;
-  content.classList.toggle('shortText');
-  content.classList.toggle('invisible');
-  content.classList.toggle('opacity-0');
-  content.classList.toggle('opacity-100');
+ let content;
+  if (event.target.classList.contains('entryContainer')) {
+    content = event.target;
+    var contentChild = content.querySelector('.contentContainer');
+    contentChild.classList.toggle('shortText');
+    contentChild.classList.toggle('invisible');
+    contentChild.classList.toggle('opacity-0');
+    contentChild.classList.toggle('opacity-100');
+    var contentCaret = content.querySelector('.fa-caret-down');
+    contentCaret.classList.toggle('rotate');
+  } else if (event.target.classList.contains('authorContainer')) {
+    content = event.target.nextElementSibling;
+    content.classList.toggle('shortText');
+    content.classList.toggle('invisible');
+    content.classList.toggle('opacity-0');
+    content.classList.toggle('opacity-100');
+    console.log(event.target);
+    var contentCaret = event.target.closest('.entryContainer').querySelector('.fa-caret-down');
+    contentCaret.classList.toggle('rotate');
+  } else if (event.target.classList == '') {
+    content = event.target.parentNode;
+    content.classList.toggle('shortText');
+    content.classList.toggle('invisible');
+    content.classList.toggle('opacity-0');
+    content.classList.toggle('opacity-100');
+    var contentCaret = event.target.closest('.entryContainer').querySelector('.fa-caret-down');
+    contentCaret.classList.toggle('rotate');
+  } 
+}
+
+function contentRotate(event) {
+  var content = event.target;
+  content.classList.toggle('rotate');
 }
