27bdab426c8484b3a5f37987dad86742336fdf2f
Eden Kupermintz <edenk@liveperson.com)>
2020-08-19 18:45:34 +0300

    more fancy

--- src/site/_includes/js/core.js
+++ src/site/_includes/js/core.js
@@ -70,3 +70,12 @@ const handleView = item => {
 
 // Apply method on each DOM element 
 ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w", "x", "y", "z"].forEach(handleView);
+
+function activateLink (event) {
+  console.log('active');
+  const prevEl = document.querySelector(`.is-active`);
+  if (prevEl) {
+    prevEl.classList.remove('is-active');
+  }
+  event.target.parentNode.classList.add('is-active');
+}
