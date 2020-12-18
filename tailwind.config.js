65fe679220e0161ccdf4f700f193bef8a452a7a3
Eden Kupermintz <edenk@liveperson.com)>
2020-12-18 17:24:20 +0200

    new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        tiffany: '#D3AAFF',
-        carmine: '#222233',
-        tangerine: '#AACCFF',
-        begonia: '#F97373',
-        quicksilver: '#AFA694',
-        sunset: '#FE5F55',
-        rich: '#080c13',
-        grollo: '#a89e8a',
-        baby: '#D7C0D0',
-        fire: '#C1292E'
+        background: '#0e141b',
+        primary: '#F97373',
+        secondary: '#5773FF',
+        text: '#FFFFFF',
+        box: '#242f42',
+        title: '#ffcb36',
       }
     }
   },
